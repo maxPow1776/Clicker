d3dc128310223401f06c670029da563c4c31bbab
Максим <maks.greb2@gmail.com>
2020-03-06 15:41:09 +0300

    Добавил Canvas и подсчет очков

--- Assets/Scripts/ConditionCollision.cs
+++ Assets/Scripts/ConditionCollision.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ConditionCollision : MonoBehaviour
 {
+    public Text scoreText;
+    private int score = 0;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("Wall"))
@@ -10,6 +14,8 @@ public class ConditionCollision : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0, 180, 0);
             else
                 transform.rotation = Quaternion.Euler(0, 0, 0);
+            score += 1;
+            scoreText.text = score.ToString();
         } else
         {
             Destroy(gameObject);
