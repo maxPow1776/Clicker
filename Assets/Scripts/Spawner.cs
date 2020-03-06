141c7c614a741b3d31ef11aef743d29868a3ca57
Максим <maks.greb2@gmail.com>
2020-03-06 16:54:04 +0300

    Теперь flowers появляются в разных местах через определенное время

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float interval;
+
+    public GameObject from;
+    public GameObject to;
+
+    private float timer;
+
+    private void FixedUpdate()
+    {
+        timer -= Time.fixedDeltaTime;
+
+        if (timer > 0)
+            return;
+
+        timer += interval;
+        var position = new Vector2(Random.Range(from.transform.position.x,
+            to.transform.position.x), Random.Range(from.transform.position.y,
+            to.transform.position.y));
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
