54e462e338ff95826df88c6ef6907e420c94a042
Максим <maks.greb2@gmail.com>
2020-03-21 16:19:09 +0300

    Поменял условия коллизии

--- Assets/Scripts/ConditionCollision.cs
+++ Assets/Scripts/ConditionCollision.cs
@@ -9,7 +9,7 @@ public class ConditionCollision : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name.Contains("Wall"))
+        if (collision.gameObject.GetComponent<BoxCollider2D>())
         {
             if (transform.rotation.y == 0)
                 transform.rotation = Quaternion.Euler(0, 180, 0);
@@ -17,7 +17,7 @@ public class ConditionCollision : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0, 0, 0);
 
         }
-        else if (collision.gameObject.name.Contains("Flower"))
+        else if (collision.gameObject.GetComponent<CircleCollider2D>())
         {
             Destroy(collision.gameObject);
             score += 1;
